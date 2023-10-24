4741214c719e4b027ce7bc87967f8c42599344df
Olatunji Longe <5619338+Olatunji-Longe@users.noreply.github.com>
2023-10-24 00:09:00 -0400

    init commit

---
+++ src/main/java/io/pathfound/projectzero/ProjectZeroApplication.java
@@ -0,0 +1,13 @@
+package io.pathfound.projectzero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectZeroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectZeroApplication.class, args);
+    }
+
+}
